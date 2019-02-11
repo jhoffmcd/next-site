5fea7a2f14860d5578fdec03460db65dc34b7f34
Jesse Hoffman <jhoffmcd@gmail.com>
2019-02-11 11:13:04 -0800

    Swap unamed exports for named ones on homepage demos

--- www/components/home/demos/code-splitting.js
+++ www/components/home/demos/code-splitting.js
@@ -1,50 +1,67 @@
-import Link from 'next/link'
+import Link from 'next/link';
 
-import Code from './code'
-import withFrame from './frame'
+import Code from './code';
+import withFrame from './frame';
 
-const IndexFile = () => <Code>{
-`import Link from 'next/link'
+const IndexFile = () => (
+  <Code>
+    {
+      `import Link from 'next/link'
 
-export default () => (
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello, this is the homepage</h1>
+      <p>I'm only 0.59 KB after gzipped.</p>
+      <Link href='/cowsay'><a>Cowsay</a></Link>
+    </div>
+  )
+}
+`
+    }
+
+  </Code>
+);
+
+const AboutFile = () => (
+  <Code>
+    {
+      `import Link from 'next/link'
+
+// only being loaded on \`/cowsay\`
+import cowsay from 'cowsay-browser'
+
+export default function CowsayPage() {
+  return (
+    <div>
+      <p>This page costs 29.8 KB after gzipped!</p>
+      <pre>
+        {cowsay.say({ text: 'I am a big package!' })}
+      </pre>
+      <Link href='/'><a>Go home</a></Link>
+    </div>
+  )
+}
+`
+    }
+
+  </Code>
+);
+
+const IndexPage = withFrame(({ A }) => (
   <div>
     <h1>Hello, this is the homepage</h1>
     <p>I'm only 0.59 KB after gzipped.</p>
-    <Link href='/cowsay'><a>Cowsay</a></Link>
+    <A tab="http://localhost:3000/cowsay">Cowsay</A>
   </div>
-)
-`
-}</Code>
+));
 
-const AboutFile = () => <Code>{
-`import Link from 'next/link'
-
-// only being loaded on \`/cowsay\`
-import cowsay from 'cowsay-browser' 
-
-export default () => <div>
-  <p>This page costs 29.8 KB after gzipped!</p>
-  <pre>
-    {cowsay.say({ text: 'I am a big package!' })}
-  </pre>
-  <Link href='/'><a>Go home</a></Link>
-</div>
-`
-}</Code>
-
-const IndexPage = withFrame(({A}) => 
-  <div>
-    <h1>Hello, this is the homepage</h1>
-    <p>{`I'm only 0.59 KB after gzipped.`}</p>
-    <A tab='http://localhost:3000/cowsay'>Cowsay</A>
-  </div>
-)
-
-const AboutPage = withFrame(({A}) => 
+const AboutPage = withFrame(({ A }) => (
   <div>
     <p>This page costs 29.8 KB after gzipped!</p>
-    <pre>{
-` _____________________
+    <pre>
+      {
+        ` _____________________
 < I am a big package! >
  ---------------------
        \\   ^__^
@@ -52,10 +69,12 @@ const AboutPage = withFrame(({A}) =>
            (__)\       )\\/\\
                ||----w |
                ||     ||`
-    }</pre>
-    <A tab='http://localhost:3000'>Go home</A>
+      }
+
+    </pre>
+    <A tab="http://localhost:3000">Go home</A>
   </div>
-)
+));
 
 export default {
   type: ['editor', 'browser'],
@@ -64,19 +83,23 @@ export default {
     editorTabs: ['pages/index.js', 'pages/cowsay.js'],
     editorMapping: {
       'pages/index.js': IndexFile,
-      'pages/cowsay.js': AboutFile
+      'pages/cowsay.js': AboutFile,
     },
   },
   browser2: {
     browserTabs: ['http://localhost:3000', 'http://localhost:3000/cowsay'],
     browserMapping: {
       'http://localhost:3000': IndexPage,
-      'http://localhost:3000/cowsay': AboutPage
+      'http://localhost:3000/cowsay': AboutPage,
     },
   },
   note: <>
     <p>
-      Every <code>import</code> you declare gets bundled and served with each page. That means pages never load unnecessary code!
+      Every
+      {' '}
+      <code>import</code>
+      {' '}
+      you declare gets bundled and served with each page. That means pages never load unnecessary code!
     </p>
-  </>
-}
+  </>,
+};
--- www/components/home/demos/file-system-routing.js
+++ www/components/home/demos/file-system-routing.js
@@ -1,30 +1,44 @@
-import Code from './code'
-import withFrame from './frame'
+import Code from './code';
+import withFrame from './frame';
 
-const IndexFile = () => <Code>{
-`import Link from 'next/link'
+const IndexFile = () => (
+  <Code>
+    {
+      `import Link from 'next/link'
 
-export default () => (
-  <div>
-    <h1>Hello Next.js 👋</h1>
-    <Link href='/about'><a>About</a></Link>
-  </div>
-)
-`}</Code>
+export default function Home() {
+  return (
+    <div>
+      <h1>Hello Next.js 👋</h1>
+      <Link href='/about'><a>About</a></Link>
+    </div>
+  )
+}
+`}
+  </Code>
+);
 
-const AboutFile = () => <Code>{
-`import Link from 'next/link'
+const AboutFile = () => (
+  <Code>
+    {
+      `import Link from 'next/link'
 
-export default () => (
-  <div>
-    <p>This is the about page</p>
-    <Link href='/'><a>Go home</a></Link>
-  </div>
-)
-`}</Code>
+export default function About() {
+  return (
+    <div>
+      <p>This is the about page</p>
+      <Link href='/'><a>Go home</a></Link>
+    </div>
+  )
+}
+`}
+  </Code>
+);
 
-const PackageFile = () => <Code lang='json'>{
-`{
+const PackageFile = () => (
+  <Code lang="json">
+    {
+      `{
   "scripts": {
     "dev": "next",
     "build": "next build",
@@ -37,21 +51,23 @@ const PackageFile = () => <Code lang='json'>{
     "react-dom": "^16.5.1"
   }
 }
-`}</Code>
+`}
+  </Code>
+);
 
-const IndexPage = withFrame(({A}) => 
+const IndexPage = withFrame(({ A }) => (
   <div>
     <h1>Hello Next.js 👋</h1>
-    <A tab='http://localhost:3000/about'>About</A>
+    <A tab="http://localhost:3000/about">About</A>
   </div>
-)
+));
 
-const AboutPage = withFrame(({A}) => 
+const AboutPage = withFrame(({ A }) => (
   <div>
     <p>This is the about page</p>
-    <A tab='http://localhost:3000'>Go home</A>
+    <A tab="http://localhost:3000">Go home</A>
   </div>
-)
+));
 
 export default {
   type: ['editor', 'browser'],
@@ -61,18 +77,33 @@ export default {
     editorMapping: {
       'pages/index.js': IndexFile,
       'pages/about.js': AboutFile,
-      'package.json': PackageFile
+      'package.json': PackageFile,
     },
   },
   browser2: {
     browserTabs: ['http://localhost:3000', 'http://localhost:3000/about'],
     browserMapping: {
       'http://localhost:3000': IndexPage,
-      'http://localhost:3000/about': AboutPage
+      'http://localhost:3000/about': AboutPage,
     },
   },
   note: <>
-    <p>Next.js will serve each file in <code>/pages</code> under a pathname matching the filename.</p>
-    <p>For example, <code>/pages/about.js</code> is served at <code>site.com/about</code>.</p>
-  </>
-}
+    <p>
+      Next.js will serve each file in
+      {' '}
+      <code>/pages</code>
+      {' '}
+      under a pathname matching the filename.
+    </p>
+    <p>
+      For example,
+      {' '}
+      <code>/pages/about.js</code>
+      {' '}
+      is served at
+      {' '}
+      <code>site.com/about</code>
+      .
+    </p>
+  </>,
+};
